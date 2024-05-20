f97fc8468939cfc303d1099bc1228794f161bd36
Kilian Tyler <github@ktyler.me>
2024-05-19 22:11:37 -0400

    fix(kubeconform): skip sops files

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -8,21 +8,29 @@ KUBERNETES_DIR=$1
 
 kustomize_args=("--load-restrictor=LoadRestrictionsNone")
 kustomize_config="kustomization.yaml"
-kubeconform_args=(
+base_kubeconform_args=(
   "-strict"
   "-ignore-missing-schemas"
-  "-skip"
-  "Secret"
   "-schema-location"
   "default"
   "-schema-location"
   "https://kubernetes-schemas.pages.dev/{{.Group}}/{{.ResourceKind}}_{{.ResourceAPIVersion}}.json"
   "-verbose"
 )
+flux_kubeconform_args=(
+  "-skip"
+  "Secret,Namespace"
+  "${base_kubeconform_args[@]}"
+)
+generic_kubeconform_args=(
+  "-skip"
+  "Secret"
+  "${base_kubeconform_args[@]}"
+)
 
 echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file; do
-  kubeconform "${kubeconform_args[@]}" "${file}"
+  kubeconform "${flux_kubeconform_args[@]}" "${file}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
     exit 1
   fi
@@ -32,7 +40,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
   echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
   kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-    kubeconform "${kubeconform_args[@]}"
+    kubeconform "${flux_kubeconform_args[@]}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
     exit 1
   fi
@@ -42,7 +50,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file; do
   echo "=== Validating kustomizations in ${file/%$kustomize_config/} ==="
   kustomize build "${file/%$kustomize_config/}" "${kustomize_args[@]}" |
-    kubeconform "${kubeconform_args[@]}"
+    kubeconform "${generic_kubeconform_args[@]}"
   if [[ ${PIPESTATUS[0]} != 0 ]]; then
     exit 1
   fi

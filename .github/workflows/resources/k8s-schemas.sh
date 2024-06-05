b14c9f946f5c35222a56eae8fa24f9eddcbf8d09
Kilian Tyler <github@ktyler.me>
2024-06-05 14:35:55 -0400

    fix: path

--- .github/workflows/resources/k8s-schemas.sh
+++ .github/workflows/resources/k8s-schemas.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-OPENAPI2JSONSCHEMABIN="docker run -i -v /home/runner/schemas/kubernetes:/out/schemas ghcr.io/yannh/openapi2jsonschema:latest"
-
 # renovate: datasource=docker depName=ghcr.io/siderolabs/kubelet
 K8S_VERSIONS="v1.30.1"
 for K8S_VERSION in $K8S_VERSIONS master; do
@@ -9,7 +7,7 @@ for K8S_VERSION in $K8S_VERSIONS master; do
   PREFIX=https://k8s-schemas.pages.dev/kubernetes/${K8S_VERSION}/_definitions.json
 
   if [ ! -d "${K8S_VERSION}" ]; then
-    $OPENAPI2JSONSCHEMABIN -o "schemas/${K8S_VERSION}" --expanded --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
-    $OPENAPI2JSONSCHEMABIN -o "schemas/${K8S_VERSION}" --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
+    openapi2jsonschema -o "schemas/kubernetes/${K8S_VERSION}" --expanded --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
+    openapi2jsonschema -o "schemas/kubernetes/${K8S_VERSION}" --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
   fi
 done

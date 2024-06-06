071424f5e34f3df6cd76fb3ac718c524eb622cd8
Kilian Tyler <github@ktyler.me>
2024-06-05 23:15:42 -0400

    K8S-3 Trunk: Check for yaml-language-server (#121)

---
+++ .github/workflows/resources/k8s-schemas.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# renovate: datasource=docker depName=ghcr.io/siderolabs/kubelet
+K8S_VERSIONS="v1.30.1"
+for K8S_VERSION in $K8S_VERSIONS master; do
+  SCHEMA=https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/api/openapi-spec/swagger.json
+  PREFIX=https://k8s-schemas.pages.dev/kubernetes/${K8S_VERSION}/_definitions.json
+
+  if [ ! -d "${K8S_VERSION}" ]; then
+    openapi2jsonschema -o "/home/runner/schemas/kubernetes/${K8S_VERSION}" --expanded --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
+    openapi2jsonschema -o "/home/runner/schemas/kubernetes/${K8S_VERSION}" --kubernetes --prefix "${PREFIX}" "${SCHEMA}"
+  fi
+done

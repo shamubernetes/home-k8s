c90cdf56222a493709261b02a8cbf62f187d580b
Kilian Tyler <github@ktyler.me>
2024-10-15 19:01:08 -0400

    feat: k8s-177 (#627)

--- .github/workflows/resources/k8s-schemas.sh
+++ .github/workflows/resources/k8s-schemas.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# renovate: datasource=docker depName=ghcr.io/siderolabs/kubelet
-K8S_VERSIONS="v1.30.2"
+# Get k8s version from system-upgrade-controller
+
+K8S_VERSIONS=$(git ls-remote --refs --tags https://github.com/kubernetes/kubernetes.git | cut -d/ -f3 | grep -e '^v1\.[0-9]\{2\}\.[0-9]\{1,2\}$' | grep -v -e '^v1\.1[0-8]\{1\}' | tail -10)
+CURRENT_VERSION=$(yq 'select(document_index == 1).spec.postBuild.substitute.KUBERNETES_VERSION' "$GITHUB_WORKSPACE"/kubernetes/apps/system-upgrade/system-upgrade-controller/ks.yaml)
+
+# Make sure the current version is in the list
+if ! echo "$K8S_VERSIONS" | grep -q "$CURRENT_VERSION"; then
+  K8S_VERSIONS="$CURRENT_VERSION"$'\n'"$K8S_VERSIONS"
+fi
+
 for K8S_VERSION in $K8S_VERSIONS master; do
   SCHEMA=https://raw.githubusercontent.com/kubernetes/kubernetes/${K8S_VERSION}/api/openapi-spec/swagger.json
   PREFIX=https://k8s-schemas.pages.dev/kubernetes/${K8S_VERSION}/_definitions.json

db6ea5c62e00d35442f1af3e4a0e4f52fdbf5d2b
Kilian Tyler <kilian@kil.dev>
2025-06-09 21:08:45 -0400

    chore: no more pg for renovate

--- kubernetes/apps/renovate/renovate/app/config/config.js
+++ kubernetes/apps/renovate/renovate/app/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
   allowScripts: true,
   assigneesFromCodeOwners: true,
   autodiscover: true,
-  autodiscoverFilter: ["shamubernetes/*", "kiliantyler/*", "zshplugins/*"],
+  autodiscoverFilter: ["shamubernetes/*", "kiliantyler/*"],
   baseDir: "/tmp/renovate",
   inheritConfig: true,
   inheritConfigFileName: "renovate.json5",
